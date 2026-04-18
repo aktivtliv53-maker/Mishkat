4d6f84b85cfbf93406e4f37d3ca18087dad42d87
aktivtliv53-maker <aktivtliv53@gmail.com>
2026-04-18 20:49:10 +0200

    Delete check_map.py

--- check_map.py
+++
@@ -1,8 +0,0 @@
-import json
-
-d = json.load(open('data/word_root_map.json', encoding='utf-8'))
-words = ['الكتاب','كتاب','للمتقين','متقين','الصلاة','صلاه','الغيب','غيب','يؤمنون','امن']
-
-for w in words:
-    result = d.get(w, "غير موجود")
-    print(f"{w}: {result}")
