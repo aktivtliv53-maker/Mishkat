89a86fca1e26fcd003003703adb9bec99064e35b
aktivtliv53-maker <aktivtliv53@gmail.com>
2026-04-19 05:49:29 +0200

    Refactor root categorization in gene spectrum engine
    Updated the compute_gene_spectrum_v5 function to use get_root_category from lexicon_v7 for root categorization.

--- utils/gene_spectrum_engine.py
+++ utils/gene_spectrum_engine.py
@@ -5,16 +5,17 @@
 
 from utils.root_engine_v7 import analyze_text_v7
 from utils.root_canonizer import canonize_root
+from utils.lexicon_v7 import get_root_category
 
 def compute_gene_spectrum_v5(text):
     """حساب الطيف الجيني للنص"""
     analysis = analyze_text_v7(text)
     roots = analysis["root_frequency"]
     
-    # تجميع الجذور حسب الفئة
+    # تجميع الجذور حسب الفئة باستخدام lexicon_v7
     categories = {}
     for root, count in roots:
-        category = analysis["root_categories"].get(root, "عام")
+        category = get_root_category(root)
         if category not in categories:
             categories[category] = 0
         categories[category] += count
